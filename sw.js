aa659d84e9461ed96493154f15e9bf87b87aa4fb
unknown <enzog2009@outlook.com>
2025-12-22 09:54:34 -0300

    18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome do cache
-const CACHE_NAME = 'epiconecta-v20-mobilepolish';
+const CACHE_NAME = 'epiconecta-v21-redesign';
 // Arquivos para armazenar em cache
 const urlsToCache = [
   './',
