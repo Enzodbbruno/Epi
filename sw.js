338fb0e9d2b52fc8b19a3e82e55a8dc0e991bea6
unknown <enzog2009@outlook.com>
2025-12-22 09:40:42 -0300

    17 teste interface

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome do cache
-const CACHE_NAME = 'epiconecta-v18-profilepolish';
+const CACHE_NAME = 'epiconecta-v20-mobilepolish';
 // Arquivos para armazenar em cache
 const urlsToCache = [
   './',
