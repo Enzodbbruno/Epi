9111b938a55acd418e45d3f9c4e80f9520e76b9a
unknown <enzog2009@outlook.com>
2026-05-28 06:55:08 -0300

    Ajusta carregamento do .env com path absoluto no index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 
 const express = require('express');
 const helmet  = require('helmet');
