384797e6afc82ec0a23d5ef4ad79d13136f78511
unknown <enzog2009@outlook.com>
2026-05-28 07:42:13 -0300

    Adiciona wrapper de captura de excecoes de importacao no api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,21 @@
-const app = require('../server/index');
-module.exports = app;
+let app;
+let initError = null;
+
+try {
+  app = require('../server/index');
+} catch (err) {
+  initError = err;
+}
+
+module.exports = (req, res) => {
+  if (initError) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).end(JSON.stringify({
+      error: 'Falha crítica na importação do servidor (Require Error)',
+      message: initError.message,
+      stack: initError.stack
+    }, null, 2));
+    return;
+  }
+  app(req, res);
+};
